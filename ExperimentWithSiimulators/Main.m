86debc1877e813ce8552ac660e5b6a7244b9da9c
slavadryga <senior.dryga2016@yandex.ru>
2022-03-05 18:35:58 +0300

    [+] debug with gnss simulation, bug fix

--- ExperimentWithSiimulators/Main.m
+++ ExperimentWithSiimulators/Main.m
@@ -1,5 +1,5 @@
 clear;
-
+close all;
 folderPath = [cd '/ScriptsFunctions'];
 addpath(folderPath);
 addpath([cd '/Records']);
@@ -11,7 +11,14 @@ folder = '/Records/';
 % -- File with 4 interseals, 4 pps, 4 clocks: -----
 % fileName = 'Interseal_Real4sv_sv16_23_10_7_1d_launch_v1.ubx'; %'\ReleaseBuild_200meters.ubx';% 'COM5_201210_093149.ubx';
 %--------------------------------------------------
-fileName = 'COM33___115200_220227_203911.ubx';
+fileName = 'COM33_220303_095213.ubx';
+fileName = 'COM33_220303_095822.ubx';
+fileName = 'COM33_220303_100600.ubx';
+fileName = 'COM33_220303_144854.ubx';
+fileName = 'COM33_220304_095833.ubx';
+fileName = 'COM33_220304_130253.ubx'; % non-constant speed of psR changing is working
+fileName = '';
+% fileName = 'ReferenceForDebugSimulation_COM53_210702_151500.ubx';
 % fileName = 'Big_Case_Interseal_2Clocks_MixedPseudo_sv_10_11_15_16_1st_launch.ubx';
 fullName = [cd folder fileName];
 
@@ -43,8 +50,14 @@ if flagWorkWithSomeCAcodesJustPsRngs
 %     PseudoCoord.svId = [7 10 16 23];
 
 % PocketZynq:
-       PseudoCoord.svId = [ 8 9 11 12 13 14];
-        
+       PseudoCoord.svId = [  9 11 12 13 14];
+    size_debug_file = size('ReferenceForDebugSimulation_COM53_210702_151500.ubx');
+    if(size_debug_file(2) == size(fileName))
+        if(fileName == 'ReferenceForDebugSimulation_COM53_210702_151500.ubx') 
+            PseudoCoord.svId =[10 22 1 21 32 3];
+        end
+    end
+
     diffPsRngs = zeros(sizeStr(2), length(PseudoCoord.svId));
     % Old Interseal (one simulator)
 %     PseudoCoord.svId = [5     7     8    13    14    18    28];
@@ -100,12 +113,13 @@ for n = 1 : sizeStr(2)
             posCnt = posCnt + 1;
 
             tow(posCnt) = ProcessedMes.rcvTow;
+            
             diffPsRngs(posCnt, 1 : length(psRngs)) = psRngs - psRngs(1);        
         end
     end
 end
     
-figure; plot(diff(tow(1 : posCnt)));
+figure; plot(tow(1 : posCnt -1), diff(tow(1 : posCnt)));
 grid on;
 title("TOW");
 ylabel("TOW, sec");
@@ -115,7 +129,7 @@ if ~flagWorkWithSomeCAcodesJustPsRngs
     ylabel("3D Error, met");
 end
 
-figure; plot(diffPsRngs(1 : posCnt, :));
+figure; plot(tow(1 : posCnt), diffPsRngs(1 : posCnt, :));
 ylabel("diffPsRngs, m");
 grid on;
 
@@ -131,7 +145,7 @@ title(leg, 'Номер псевдолита');
 figErr = figure;
 emitted_rng_diff = 0 : 1000 : 0;
 % plot(diffPsRngs(1 : posCnt, :) - emitted_rng_diff);
-plot(diffPsRngs(1 : 150, :) - emitted_rng_diff);
+plot(diffPsRngs(1 : posCnt, :));% - emitted_rng_diff);
 xlabel("t, сек");
 ylabel('{\Delta}R, м');
 grid on;
@@ -140,7 +154,8 @@ title(leg, 'Номер псевдолита');
 leg.NumColumnsMode = 'manual';
 led.NumColumns = 2;
 
-figure; plot(diff(diffPsRngs));
+speed_diffPsRngs = diff(diffPsRngs(1 : posCnt, :));
+figure; plot(tow(1 : posCnt -1), speed_diffPsRngs);
 xlabel("t, сек");
 ylabel('diff(R), м');
 grid on;
@@ -174,9 +189,45 @@ if stdPpsInMet - abs(ppsErrorExp) > 0
    fprintf("Errors of pseudoranges satisfy 1PPS errors \n"); 
 end
 
-disp ("Ublox Diff PsRnges ");
-diffPsRngs(20, :) - diffPsRngs(20, 1)
+% disp ("Ublox Diff PsRnges ");
+% diffPsRngs(20, :) - diffPsRngs(20, 1)
 % 
 % xlabel('t, сек')
 % ylabel('3D error, м')
 % grid on;
+
+%=== find diffPsRng for given TOW: ===
+TOW = 487899;%487108;
+
+% diff_arm = [-273 -123 163.2 277.2 -588];
+diff_arm = [-27.6 228 354 -537.6 -274.8];
+fprintf("Arm diff = "); fprintf("%d ", diff_arm - diff_arm(1));
+
+idx = find(round(tow) == TOW);
+% idx = idx + 50;
+fprintf("\nUblox:\nTOW = %d\n", tow(idx(1))); fprintf( "diffPsRngs = %d\n", ...
+                                                diffPsRngs(idx(1), :));
+delta_calc = round(tow(idx)) - tow(idx);
+d_psR_err = speed_diffPsRngs(idx, :) * (delta_calc - 7);
+
+
+% psRng_arm = [816175360 701092200 672325680 704752960 717769800 743740920]
+% / 1e10 * 3e8; % 107
+psRng_arm = [701083160 672321560 704758360 717779080 743721400] / 1e10 * 3e8; % 108
+psRng_arm = [670463040 709531600 725498720 730029360 765041400] / 1e10 * 3e8;
+fprintf("ARM:\n"); fprintf("diffPsRngs = %d\n", psRng_arm - psRng_arm(1));
+fprintf("double diff: ");
+disp(diffPsRngs(idx(1), :) - ((psRng_arm - psRng_arm(1)) - d_psR_err));
+
+vvv = [150 200 250 300 350];
+ch_num = 3 : 7;
+prev_a = zeros(1, 5);
+for i = 1 : 100
+    if i == 99 
+        b = 3;
+    end
+    a = prev_a + vvv + i * 0.1 * ch_num; 
+    prev_a = a;
+end
+fprintf("preva = %d, a = %d\n", prev_a, a);
+fprintf("delta = %d\n", a)
